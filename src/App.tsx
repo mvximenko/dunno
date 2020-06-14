61c1ac09d0e13c4dfa9292ebcfaf8ec8fcd4bafc
mvximenko <mr.mxmnk@gmail.com>
2020-06-14 22:11:18 +0900

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Tv from './pages/Tv';
+import Movies from './pages/Movies';
+import Title from './pages/Title';
+import Person from './pages/Person';
 import { GlobalStyle } from './GlobalStyles';
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      Dunno
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/tv' component={Tv} />
+        <Route exact path='/movie' component={Movies} />
+        <Route exact path='/tv/:titleId' component={Title} />
+        <Route exact path='/movie/:titleId' component={Title} />
+        <Route exact path='/person/:personId' component={Person} />
+      </Switch>
     </>
   );
 };
