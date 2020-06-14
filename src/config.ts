d65a9d250264b3d41b3aba5649182dccc558c63a
mvximenko <mr.mxmnk@gmail.com>
2020-06-14 22:55:21 +0900

    Add config.ts

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const API_KEY = process.env.REACT_APP_API_KEY;
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+// sizes: w300, w780, w1280, original
+export const BACKDROP_SIZE = 'w1280';
+
+// w92, w154, w185, w342, w500, w780, original
+export const POSTER_SIZE = 'w500';
