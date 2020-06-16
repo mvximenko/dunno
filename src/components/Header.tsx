d5f72bca0539f2ccb304b847c418160cedf4cf04
mvximenko <mr.mxmnk@gmail.com>
2020-06-16 21:33:26 +0900

    Update Header and HeaderStyles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,58 @@
-import React from 'react';
-import Icon from '../assets/menu.png';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Menu from '../assets/menu.png';
+import Profile from '../assets/profile.png';
+import {
+  StyledHeader,
+  Logo,
+  LogoLink,
+  Nav,
+  NavLink,
+  MenuIcon,
+  ProfileIcon,
+  Drawer,
+  DrawerLink,
+} from './HeaderStyles';
 
-import { StyledHeader, Logo, LogoLink, MenuIcon } from './HeaderStyles';
+const Header: React.FC = () => {
+  const [drawer, setDrawer] = useState(false);
+  return (
+    <>
+      <StyledHeader>
+        <Logo>
+          <LogoLink to='/'>dunno</LogoLink>
+        </Logo>
 
-const Header: React.FC = () => (
-  <StyledHeader>
-    <Logo>
-      <LogoLink to='/'>dunno</LogoLink>
-    </Logo>
-    <MenuIcon src={Icon} />
-  </StyledHeader>
-);
+        <Nav>
+          <NavLink exact to='/tv'>
+            TV Shows
+          </NavLink>
+          <NavLink exact to='/movie'>
+            Movies
+          </NavLink>
+          <NavLink exact to='/randomizer'>
+            Randomizer
+          </NavLink>
+          <NavLink exact to='/favorites'>
+            Favorites
+          </NavLink>
+        </Nav>
+
+        <Link to='/signin'>
+          <ProfileIcon src={Profile} />
+        </Link>
+        <MenuIcon src={Menu} onClick={() => setDrawer(!drawer)} />
+      </StyledHeader>
+
+      <Drawer active={drawer} onClick={() => setDrawer(!drawer)}>
+        <DrawerLink to='/tv'>TV Shows</DrawerLink>
+        <DrawerLink to='/movie'>Movies</DrawerLink>
+        <DrawerLink to='/randomizer'>Randomizer</DrawerLink>
+        <DrawerLink to='/favorites'>Favorites</DrawerLink>
+        <DrawerLink to='/signin'>Sign In</DrawerLink>
+      </Drawer>
+    </>
+  );
+};
 
 export default Header;
--- src/components/HeaderStyles.ts
+++ src/components/HeaderStyles.ts
@@ -1,30 +1,101 @@
-import styled from 'styled-components';
-import { Link as RouterLink } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+import { Link as RouterLink, NavLink as RouterNavLink } from 'react-router-dom';
+import sizes from '../sizes';
 
-export const StyledHeader = styled.header`
-  width: 100%;
-  height: 10vw;
-  display: flex;
-  padding: 0 5vw;
-  position: relative;
-  z-index: 5;
-  align-items: center;
-  box-sizing: border-box;
+const blur = css`
   background-color: rgba(0, 0, 0, 0.8);
   -webkit-backdrop-filter: blur(5px);
   backdrop-filter: blur(5px);
 `;
 
+export const StyledHeader = styled.header`
+  width: 100%;
+  height: 36px;
+  display: flex;
+  padding: 0 5vw;
+  position: relative;
+  align-items: center;
+  box-sizing: border-box;
+  z-index: 5;
+  ${blur};
+  ${[sizes.up('xs')]} {
+    height: 40px;
+    padding: 0 3vw;
+  }
+`;
+
 export const Logo = styled.div`
   width: 100%;
+  ${[sizes.up('sm')]} {
+    width: auto;
+  }
 `;
 
 export const LogoLink = styled(RouterLink)`
-  font-size: 6vw;
+  font-size: 22px;
   font-weight: 600;
   font-family: sans-serif;
 `;
 
+export const Nav = styled.nav`
+  display: none;
+  ${[sizes.up('sm')]} {
+    width: 100%;
+    display: block;
+    text-align: center;
+  }
+`;
+
+export const NavLink = styled(RouterNavLink)`
+  margin: 0 2vw;
+  color: #aeaeae;
+  font-size: 14px;
+  font-weight: 400;
+  transition: all 350ms;
+  &:hover,
+  &.active {
+    color: #fff;
+  }
+`;
+
 export const MenuIcon = styled.img`
-  width: 1.5rem;
+  width: 24px;
+  flex-shrink: 0;
+  ${[sizes.up('sm')]} {
+    display: none;
+  }
+`;
+
+export const ProfileIcon = styled.img`
+  display: none;
+  ${[sizes.up('sm')]} {
+    display: block;
+    width: 26px;
+    cursor: pointer;
+  }
+`;
+
+export const Drawer = styled.div<{ active: boolean }>`
+  top: 0;
+  right: 0;
+  width: 0;
+  height: 100%;
+  padding: 60px 0;
+  position: fixed;
+  white-space: nowrap;
+  z-index: 2;
+  ${blur}
+  ${({ active }) =>
+    active &&
+    `
+    width: 100%;
+  `}
+`;
+
+export const DrawerLink = styled(RouterLink)`
+  display: block;
+  padding: 10px 0;
+  font-size: 28px;
+  text-align: center;
+  text-decoration: none;
 `;
