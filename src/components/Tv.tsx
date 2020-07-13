ddb41cad61a7a2ea6182761135cf58917e1066f9
mvximenko <mr.mxmnk@gmail.com>
2020-07-13 18:51:40 +0900

    Update Tv

--- src/components/Tv.tsx
+++ src/components/Tv.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import List from './list/List';
+import Spinner from './layout/Spinner';
+import { useFetchList } from '../store/list/listActions';
+import listReducer from '../store/list/listReducer';
 
 const Tv: React.FC = () => {
-  return <>TV</>;
+  const initialState = { page: 1, type: 'tv', titles: [] };
+
+  const [pop, popDispatch] = useReducer(listReducer, {
+    ...initialState,
+    category: 'popular',
+  });
+  const [top, topDispatch] = useReducer(listReducer, {
+    ...initialState,
+    category: 'top_rated',
+  });
+  const [ont, ontDispatch] = useReducer(listReducer, {
+    ...initialState,
+    category: 'on_the_air',
+  });
+  const [air, airDispatch] = useReducer(listReducer, {
+    ...initialState,
+    category: 'airing_today',
+  });
+
+  useFetchList(pop, popDispatch);
+  useFetchList(top, topDispatch);
+  useFetchList(ont, ontDispatch);
+  useFetchList(air, airDispatch);
+
+  return (
+    <>
+      {ont.titles.length > 0 ? (
+        <>
+          <List titles={pop} dispatch={popDispatch} />
+          <List titles={top} dispatch={topDispatch} />
+          <List titles={ont} dispatch={ontDispatch} />
+          <List titles={air} dispatch={airDispatch} />
+        </>
+      ) : (
+        <Spinner />
+      )}
+    </>
+  );
 };
 
 export default Tv;
