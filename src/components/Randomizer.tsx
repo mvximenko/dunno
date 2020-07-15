592bc6949a84035967a542ef3611c46fe51794e2
mvximenko <mr.mxmnk@gmail.com>
2020-07-15 13:55:08 +0900

    Update Randomizer

--- src/components/Randomizer.tsx
+++ src/components/Randomizer.tsx
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { API_URL, API_KEY } from '../config';
+import { Link } from 'react-router-dom';
+import PosterPng from '../assets/poster.png';
+import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+
+interface Genres {
+  [x: string]: any[];
+}
+
+interface Title {
+  id: number;
+  name: string;
+  title: string;
+  poster_path: string;
+}
 
 const Randomizer: React.FC = () => {
   const [mediaType, setMediaType] = useState('');
-  const [genres, setGenres] = useState<any>({});
-  const [titles, setTitles] = useState<any>([]);
+  const [genres, setGenres] = useState<Genres>({ tv: [], movie: [] });
+  const [title, setTitle] = useState<Title>({
+    id: 0,
+    name: '',
+    title: '',
+    poster_path: '',
+  });
 
   useEffect(() => {
     fetch(`${API_URL}genre/tv/list?api_key=${API_KEY}&language=en-US`)
@@ -21,19 +39,42 @@ const Randomizer: React.FC = () => {
   useEffect(() => {
     if (mediaType) {
       const page = Math.floor(Math.random() * 5) + 1;
-      const random = Math.floor(Math.random() * genres[mediaType].length) + 1;
+      const random = Math.floor(Math.random() * genres[mediaType].length);
       const genre = genres[mediaType][random].id;
       fetch(
         `${API_URL}discover/${mediaType}?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=${page}&with_genres=${genre}`
       )
         .then((res: Response) => res.json())
-        .then((data) => {
-          setTitles([...data.results]);
+        .then(({ results }) => {
+          let title = null;
+          while (!title) {
+            title = results[Math.floor(Math.random() * results.length)];
+            if (title.poster_path) {
+              setTitle(title);
+            }
+          }
         });
+      setMediaType('');
     }
   }, [mediaType, genres]);
 
-  return <button onClick={() => setMediaType('tv')}></button>;
+  const { poster_path, name, title: t, id } = title;
+  return (
+    <>
+      <Link to={poster_path ? `${name ? 'tv' : 'movie'}/${id}` : `randomizer`}>
+        <img
+          alt={name ? name : t}
+          src={
+            poster_path
+              ? `${IMAGE_BASE_URL}${POSTER_SIZE}${poster_path}`
+              : PosterPng
+          }
+        />
+      </Link>
+      <button onClick={() => setMediaType('tv')}>TV</button>
+      <button onClick={() => setMediaType('movie')}>MOVIE</button>
+    </>
+  );
 };
 
 export default Randomizer;
